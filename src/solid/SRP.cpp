6693a8ebdac78ebaf3924950c3b18bd429200228
Krzysztof Urban <k.urban@g.pl>
2020-04-11 11:37:35 +0200

    Remove generated text

--- src/solid/SRP.cpp
+++ src/solid/SRP.cpp
@@ -5,24 +5,7 @@
 #include <boost/lexical_cast.hpp>
 #include <utility>
 #include <vector>
-"bits/stl_algobase.h"
-"bits/allocator.h"
-"bits/stl_construct.h"
-"bits/stl_uninitialized.h"
-"bits/stl_vector.h"
-"bits/stl_bvector.h"
-"bits/range_access.h"
-"bits/vector.tcc"
 #include <fstream>
-"bits/fstream.tcc"
-"bits/fstream.tcc"
-"bits/codecvt.h"
-"bits/fstream.tcc"
-"x86_64-w64-mingw32/bits/c++config.h"
-"stdio.h"
-"x86_64-w64-mingw32/bits/basic_file.h"
-"bits/fstream.tcc"
-"bits/fstream.tcc"
 
 using namespace std;
 using namespace boost;
