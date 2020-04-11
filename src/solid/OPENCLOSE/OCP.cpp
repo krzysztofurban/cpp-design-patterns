0948a5688033ff497f0a259247aa121f7b4ac2e3
Krzysztof Urban <k.urban@g.pl>
2020-04-11 11:14:54 +0200

    Initial commit

---
+++ src/solid/OPENCLOSE/OCP.cpp
@@ -0,0 +1,124 @@
+//
+// Created by Urban on 11.04.2020.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+enum class Color
+{
+	red, green, blue
+};
+
+enum class Size
+{
+	small, medium, large
+};
+
+struct Product
+{
+	string name;
+	Color color;
+	Size size;
+};
+
+struct ProductFilter
+{
+	static vector<Product*> by_color(vector<Product*> items, Color color)
+	{
+		vector < Product * > result;
+		for (auto& i : items)
+		{
+			if (i->color == color) result.push_back(i);
+		}
+		return result;
+	}
+};
+
+template<typename T>
+struct Specification
+{
+	virtual bool is_satisfied(T* item) = 0;
+};
+
+template<typename T>
+struct Filter
+{
+	virtual vector<T*> filter(vector<T*> items, Specification<T>& spec) = 0;
+};
+
+struct BetterFilter : Filter<Product>
+{
+	vector<Product*> filter(vector<Product*> items, Specification<Product>& spec) override
+	{
+		vector < Product * > result;
+		for (auto& item : items)
+			if (spec.is_satisfied(item))
+				result.push_back(item);
+		return result;
+	}
+};
+
+struct ColorSpecification : Specification<Product>
+{
+	Color color;
+	explicit ColorSpecification(Color color) : color(color)
+	{
+	}
+
+	bool is_satisfied(Product* item) override
+	{
+		return item->color == this->color;
+	}
+};
+
+struct SizeSpecification : Specification<Product>
+{
+	Size size;
+	explicit SizeSpecification(Size size) : size{ size }
+	{
+	}
+
+	bool is_satisfied(Product* item) override
+	{
+		return item->size == this->size;
+	}
+};
+
+template<typename T>
+struct AndSpecification : Specification<T>
+{
+	Specification<T>& first;
+	Specification<T>& second;
+
+	AndSpecification(Specification<T>& first, Specification<T>& second) : first{ first }, second{ second }
+	{
+	};
+
+	bool is_satisfied(T* item) override
+	{
+		return first.is_satisfied(item) && second.is_satisfied(item);
+	}
+};
+
+int main()
+{
+	Product apple{ "Apple", Color::green, Size::small };
+	Product tree{ "Tree", Color::green, Size::medium };
+	Product house{ "House", Color::blue, Size::medium };
+	vector < Product * > product_vector{ &apple, &tree, &house };
+	BetterFilter bf;
+	ColorSpecification green{ Color::green };
+	SizeSpecification mid{ Size::medium };
+	AndSpecification<Product> green_and_medium(green, mid);
+
+	auto items = bf.filter(product_vector, green_and_medium);
+	for (auto& item : items)
+	{
+		cout << item->name << endl;
+	}
+	return 0;
+}
---
+++ src/solid/SRP/SRP.cpp
@@ -0,0 +1,48 @@
+//
+// Created by Urban on 11.04.2020.
+//
+
+#include <boost/lexical_cast.hpp>
+#include <utility>
+#include <vector>
+#include <fstream>
+
+using namespace std;
+using namespace boost;
+
+struct Journal
+{
+	string title;
+	vector<string> entries;
+
+	Journal(string title) : title{ std::move(title) }
+	{
+	};
+
+	void add_entry(const string& entry)
+	{
+		static int count = 1;
+		entries.push_back(to_string(count++) + ": " + entry);
+	}
+};
+
+struct PersistenceManager
+{
+	static void save(const Journal& journal, const string& filename)
+	{
+		ofstream ofs(filename);
+		for (auto& e : journal.entries) ofs << e << endl;
+	}
+};
+
+int main()
+{
+	Journal journal{ "Dear Diary" };
+	journal.add_entry("I ate a bug");
+	journal.add_entry("I cried today");
+	journal.add_entry("I cried today");
+	journal.add_entry("I cried today");
+	journal.add_entry("I cried today");
+	PersistenceManager::save(journal, "PMTest.txt");
+	return 0;
+}
